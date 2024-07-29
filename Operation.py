7d9b86f01f74aac314f3bd3a52c97b1e9141ed3a
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-07-29 12:08:49 +0200

    its a working regex

--- Operation.py
+++ Operation.py
@@ -1,16 +1,21 @@
 import regex as re
 class Operation:
     def __init__(self, line) -> None:
-        pattern = re.compile(r'G\d+(?:A(?P<A>-?\d*\.?\d+))?(?:X(?P<X>-?\d*\.?\d+))?(?:Y(?P<Y>-?\d*\.?\d+))?(?:Z(?P<Z>-?\d*\.?\d+))?(?:F(?P<F>-?\d*\.?\d+))?')
-        match=pattern.search(line)
-        print(match)
-        # self.X=match['X']
-        # self.Y=match['Y']
-        # self.Z=match['Z']
-        # self.A=match['A']
-        # self.F=match['F']
-        if match:
-            print({k: v for k, v in match.groupdict().items() if v is not None})
+        dict_of_values= self.parse_line(line)
+        # print(dict_of_values)
+        self.X=dict_of_values['X']
+        self.Z=dict_of_values['Z']
+        self.A=dict_of_values['A']
+        self.F=dict_of_values['F']
+        self.operation=dict_of_values['operation']
 
+    def parse_line(self,line):
+        dict_of_values = {}
+        dict_of_values['operation'] = re.search(r'[A-Z]\d+', line).group(0)
+        dict_of_values['X'] = re.search(r'X\d+\.\d+', line).group(0)
+        dict_of_values['Z'] = re.search(r'Z\d+\.\d+', line).group(0)
+        dict_of_values['A'] = re.search(r'A\d+\.\d+', line).group(0)
+        dict_of_values['F'] = re.search(r'F\d+\.\d+', line).group(0)
+        return dict_of_values
 Operation('G01X114.6109Z27.0517A18.8177674F1600.0')
  
