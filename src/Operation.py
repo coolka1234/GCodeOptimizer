f6b4a7e47fce7363847cd7b5621f13ce38214e66
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-07-31 15:26:35 +0200

    good progress on f calc

--- src/Operation.py
+++ src/Operation.py
@@ -1,5 +1,6 @@
 import regex as re
 import global_vars
+from local_logging import logger
 class Operation:
     def __init__(self, line) -> None:
         dict_of_values= self.parse_line(line)
@@ -9,17 +10,25 @@ class Operation:
         self.A=0 if dict_of_values['A']==None else float(dict_of_values['A'][1:])
         self.F=global_vars.global_F if dict_of_values['F']==None else float(dict_of_values['F'][1:])
         self.operation=global_vars.global_operation if dict_of_values['operation']==None else dict_of_values['operation']
-
+        if self.Z is not None:
+            global_vars.global_Z=self.Z
+        if self.F is not None:
+            global_vars.global_F=self.F
+        if self.operation is not None:
+            global_vars.global_operation=self.operation
+        if self.X is not None:
+            global_vars.global_X=self.X
+        logger.debug(f"Operation: {self}")
     def parse_line(self,line):
         dict_of_values = {}
         dict_of_values['operation'] = re.search(r'[A-Z]\d+', line).group(0)
-        x_match = re.search(r'X\d+\.\d+', line)
+        x_match = re.search(r'X-?\d+\.\d+', line)
         dict_of_values['X'] = x_match.group(0) if x_match else None
         
-        z_match = re.search(r'Z\d+\.\d+', line)
+        z_match = re.search(r'Z-?\d+\.\d+', line)
         dict_of_values['Z'] = z_match.group(0) if z_match else None
         
-        a_match = re.search(r'A\d+\.\d+', line)
+        a_match = re.search(r'A-?\d+\.\d+', line)
         dict_of_values['A'] = a_match.group(0) if a_match else None
         
         f_match = re.search(r'F\d+\.\d+', line)
@@ -28,5 +37,5 @@ class Operation:
 
     def __str__(self) -> str:
         return f'X: {self.X}, Z: {self.Z}, A: {self.A}, F: {self.F}, Operation: {self.operation}'    
-print(str(Operation('G01X114.6109Z27.0517A18.8177674F1600.0')))
+print(str(Operation('G01A18.8177674F1600.0')))
  
--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,7 @@
 from math import log
 import math
 from file_operations import read_nc_file
+import global_vars
 from local_logging import logger
 from Operation import Operation
 import regex as re
@@ -21,27 +22,52 @@ def get_and_write(file_path):
 def is_line_of_interest(line):
     if '(' in line or ')' in line:
         return False
-    pattern = r'[AXYZG]'
+    pattern = r'[AXYZ]'
     return bool(re.search(pattern, line))
  
-def calcualte_F(line):
+# def calcualte_F(line):
+#     operation_line=Operation(line)
+#     A=operation_line.A
+#     F=operation_line.F
+#     X=operation_line.X
+#     Z=operation_line.Z
+#     effective_radius = Z
+# #     distance = math.sqrt(X**2 + (effective_radius * A)**2)
+#     logger.debug(f"Distance: {distance} for X: {X}, Z: {Z}, A: {A}")
+#     if F is not None:
+#         original_F = F
+#         F = original_F / distance * X if distance != 0 else original_F
+#         logger.debug(f"Original F: {original_F}, New F: {F}")
+#     return F
+
+def calculate_F(line):
     operation_line=Operation(line)
     A=operation_line.A
     F=operation_line.F
     X=operation_line.X
     Z=operation_line.Z
     effective_radius = Z
-    distance = math.sqrt(X**2 + (effective_radius * A)**2)
-    if F is not None:
-        original_F = F
-        F = original_F / distance * X if distance != 0 else original_F
-    return F
+    if X is None or X == 0:
+        effective_radius = Z
+        distance = abs(effective_radius * A)
+    else:
+        effective_radius = Z
+        distance = math.sqrt(X**2 + (effective_radius * A)**2)
+    if distance != 0:
+        adjusted_F = F / distance * global_vars.global_X
+        logger.debug(f"Distance: {distance} for X: {X}, Z: {Z}, A: {A}, F: {F}")
+    else:
+        adjusted_F = F
+    
+    return adjusted_F
 
 def replace_f_in_line(f_value, line):
     output_string = re.sub(r'F\d+(\.\d+)?', 'F'+str(f_value), line)
+    logger.debug(f"Old line: {line}")
+    logger.debug(f"New line: {output_string}")
     return output_string
 
 def handle_the_line(line):
-    F=calcualte_F(line)
+    F=calculate_F(line)
     new_line=replace_f_in_line(F, line)
     return new_line
