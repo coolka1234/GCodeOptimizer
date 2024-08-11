0246a81e6c948abdaa6c6230992bbd117e0d4274
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-08-11 17:10:12 +0200

    changes to ui

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from src.gui.main_window import Ui_MainWindow
 from src.backend.functions import handle_the_line
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QFileDialog
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
-        self.pushButtonConfirm.clicked.connect(self.handle_button_click)
+        self.pushButtonConfirm.clicked.connect(self.open_file)
+   
+    def open_file(self):
+        self.lineEditFilePath.setText(QFileDialog.getOpenFileName(self, 'Open file', '')[0])
 
-    def handle_button_click(self):
-        file_path = self.lineEditFilePath.text()
-        handle_the_line(file_path)
 
 
 if __name__ == '__main__':
