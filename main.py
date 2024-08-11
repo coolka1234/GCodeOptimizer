45cbff206f066f9adae02a40b65e96a21e68e181
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-08-11 18:16:29 +0200

    more exception handling

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.pushButtonChooseFile.clicked.connect(self.open_file)
         self.pushButtonChooseSave.clicked.connect(self.choose_save_path)
         self.pushButtonConfirm.clicked.connect(self.execute)
+        self.comboBoxLoggingLevel.currentIndexChanged.connect(self.set_logging_level)
+        self.comboBoxLanguage.currentIndexChanged.connect(self.set_language)
+        self.initialize_log_combobox()
+        self.initialize_language_combobox()
         
    
     def open_file(self):
@@ -25,18 +29,27 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         file_path= self.lineEditFilePath.text()
         if file_path == '':
             QMessageBox.critical(self, 'Error', 'Please choose a file')
+            return
         if not file_path.endswith('.nc'):
             QMessageBox.critical(self, 'Error', 'The file is not a .nc file')
+            return
         if not os.path.exists(file_path):
             QMessageBox.critical(self, 'Error', 'The file does not exist')
+            return
         if not os.path.exists(save_path):
             QMessageBox.critical(self, 'Error', 'The save path does not exist')
+            return
         if save_path == '':
             save_path = file_path.replace('.nc', '_processed.nc')
+            logger.warning(f"Save path not provided, saving to {save_path}")
         else:
             save_path = save_path + '/' + file_path.split('/')[-1].replace('.nc', '_processed.nc')
-        main(file_path, save_path) 
-
+        try:
+            main(file_path, save_path) 
+            QMessageBox.information(self, 'Success', 'File processed successfully')
+        except Exception as e:
+            logger.error(f"Error processing file: {e}")
+            QMessageBox.critical(self, 'Error', 'Error processing file')
     def initialize_log_combobox(self):
         self.comboBoxLoggingLevel.addItem('DEBUG')
         self.comboBoxLoggingLevel.addItem('INFO')
